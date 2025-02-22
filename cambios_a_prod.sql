a8a82196510c56b037cad379a6e4d54f0f1fbcaa
Kenny Rodriguez <91978916+kappsme@users.noreply.github.com>
2025-02-22 15:59:05 -0600

    Proceso de Venta Parte 1

--- cambios_a_prod.sql
+++ cambios_a_prod.sql
@@ -3,3 +3,24 @@ INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `
 INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `html_type`, `category`, `customizable`, `required`, `system`, `default_value`) VALUES ('53', '2', 'SINPE', '1', 'CHECKBOX', 'PAGOS', '1', '1', '1', '1');
 INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `html_type`, `category`, `customizable`, `required`, `system`, `default_value`) VALUES ('54', '2', 'Tarjeta', '1', 'CHECKBOX', 'PAGOS', '1', '1', '1', '1');
 INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `html_type`, `category`, `customizable`, `required`, `system`, `default_value`) VALUES ('55', '2', 'A Crédito', '1', 'CHECKBOX', 'PAGOS', '1', '1', '1', '1');
+
+
+
+create table tx.tnd_ventas
+(id int primary key auto_increment,
+id_kapp int,
+id_cliente int,
+fecha datetime default now(),
+id_usuario int);
+
+ create table tx.tnd_ventas_datos
+ (id int primary key auto_increment,
+ id_venta int,
+ id_producto int,
+ unidades int,
+ precio_unitario decimal,
+ id_lote int,
+ monto_total decimal,
+ descuento decimal,
+ impuesto decimal
+ );
