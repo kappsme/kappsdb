4ebae0901d1602f49ccb0fa121488ce78b8880f5
Kenny Rodriguez <91978916+kappsme@users.noreply.github.com>
2025-02-24 20:51:56 -0600

    TX-81  TX - TND _ Agregar modulo de carga de Gastos y Resumen - (Cierre) p1

--- cambios_a_prod.sql
+++ cambios_a_prod.sql
@@ -28,3 +28,17 @@ id_usuario int);
 INSERT INTO `tx`.`ctz_base` (`id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('2', '3', 'NOMBRE', '1', '1', '1');
 INSERT INTO `tx`.`ctz_base` (`id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('2', '3', 'APELLIDO', '1', '1', '1');
 INSERT INTO `tx`.`ctz_base` (`id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('2', '3', 'TELEFONO', '2', '1', '1');
+
+INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `html_type`, `category`, `customizable`, `required`, `system`, `default_value`) VALUES ('56', '2', 'Sección de Gastos', '1', 'CHECKBOX', 'PARAMETERS', '1', '1', '1', '0');
+
+
+UPDATE `kapps_db`.`tx_params_cat` SET `category` = 'LABELS' WHERE (`id` = '50');
+UPDATE `kapps_db`.`tx_params_cat` SET `category` = 'LABELS' WHERE (`id` = '37');
+
+INSERT INTO `tx`.`ctz_module_screens` (`id`, `id_module_cat`, `screen_name`, `is_active`) VALUES ('5', '2', 'GASTOS', '1');
+
+INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('10', '2', '5', 'FECHA', '3', '1', '1');
+
+INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('11', '2', '5', 'CATEGORIA', '8', '1', '1');
+INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('12', '2', '5', 'DESCRIPCION', '1', '1', '1');
+INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('13', '2', '5', 'MONTO', '2', '1', '1');
