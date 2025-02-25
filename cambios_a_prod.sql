5e7088d743db584e8369237a0cd70f5dc30527c2
Kenny Rodriguez <91978916+kappsme@users.noreply.github.com>
2025-02-24 22:38:17 -0600

    Saving Advance

--- cambios_a_prod.sql
+++ cambios_a_prod.sql
@@ -42,3 +42,17 @@ INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_n
 INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('11', '2', '5', 'CATEGORIA', '8', '1', '1');
 INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('12', '2', '5', 'DESCRIPCION', '1', '1', '1');
 INSERT INTO `tx`.`ctz_base` (`id`, `id_module_cat`, `id_module_screen`, `field_name`, `id_field_type`, `is_customizable`, `is_active`) VALUES ('13', '2', '5', 'MONTO', '2', '1', '1');
+
+
+SELECT id_elemento,
+GROUP_CONCAT( if( ctz.field_name = 'FECHA', IF(ctz.id_field_type=6,value,valor), NULL ) ) AS 'FECHA',
+GROUP_CONCAT( if( ctz.field_name = 'CATEGORIA', IF(ctz.id_field_type=6,value,valor), NULL ) ) AS 'CATEGORIA',
+GROUP_CONCAT( if( ctz.field_name = 'DESCRIPCION', IF(ctz.id_field_type=6,value,valor), NULL ) ) AS 'DESCRIPCION',
+GROUP_CONCAT( if( ctz.field_name = 'MONTO', IF(ctz.id_field_type=6,value,valor), NULL ) ) AS 'MONTO'
+			-- ctz.field_name, IF(ctz.id_field_type=6,value,valor) valor
+                from tx.elementos_datos ed left join tx.elementos e on e.id=ed.id_elemento
+                left join tx.ctz on ctz.id=ed.id_ctz
+                left join tx.ctz_fto fto on valor=fto.id 
+ where e.id_kapp=1
+and e.id in (select id_elemento from tx.elementos_datos where id_ctz in (10,11,12,13))
+group by id_elemento order by id_elemento desc limit 10
