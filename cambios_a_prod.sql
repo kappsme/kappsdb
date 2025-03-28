9fff53ead186510f98fd9de42113d5ba26dd0d05
Kenny Rodriguez <91978916+kappsme@users.noreply.github.com>
2025-03-27 22:14:22 -0600

    INV Ingresos DB

--- cambios_a_prod.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `inv_proveedores_conf_ingreso` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_proveedor` int DEFAULT NULL,
-  `id_producto` int DEFAULT NULL,
-  `unidades` int DEFAULT NULL,
-  `id_usuario` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
