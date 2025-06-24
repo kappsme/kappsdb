89dda8e7b30e2de126ddc0d5a793c2fbf43fae0f
Kenny Rodriguez <91978916+kappsme@users.noreply.github.com>
2025-06-23 22:42:23 -0600

    Update cambios_a_prod.sql

--- cambios_a_prod.sql
+++ cambios_a_prod.sql
@@ -5,3 +5,8 @@ codico varchar(12),
 estado int default 1,
 fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 fecha_uso timestamp);
+
+
+
+INSERT INTO `kapps_db`.`tx_params_cat` (`id`, `module_id`, `nombre`, `estado`, `html_type`, `category`, `customizable`, `required`, `system`, `default_value`) VALUES ('65', '0', 'Vencimiento de Sesion', '1', 'CHECKBOX', 'PARAMETERS', '1', '1', '1', '1');
+INSERT INTO `kapps_db`.`kapps_modules_cat` (`id`, `type`, `name`, `description`, `active`, `code`) VALUES ('0', 'MAIN', 'CONFIGURACION', 'Configuración General de la KAPP', '1', 'man');
